cf1066ee7499ac132331442ebaf5dac6eda7d8f9
Alexey <alexeypipchuk@gmail.com>
2020-04-19 19:00:55 +0700

    add limit arg to get cafe request

--- app/src/main/java/com/example/takeaway/feature/feed/data/api/TakeawayApi.kt
+++ app/src/main/java/com/example/takeaway/feature/feed/data/api/TakeawayApi.kt
@@ -3,9 +3,10 @@ package com.example.takeaway.feature.feed.data.api
 import com.example.takeaway.feature.feed.data.model.CafeListResponse
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface TakeawayApi {
 
-    @GET("cafe?type=closed")
-    fun getCafeList(): Single<CafeListResponse>
+    @GET("cafe")
+    fun getCafeList(@Query("limit") limit: String = "100"): Single<CafeListResponse>
 }
