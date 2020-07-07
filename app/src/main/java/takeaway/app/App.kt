5691f2f91ffdf5d533497996dd94b7adc9a18954
Alexey <alexeypipchuk@gmail.com>
2020-07-07 20:35:36 +0700

    add app metrica analytics sdk

--- app/src/main/java/takeaway/app/App.kt
+++ app/src/main/java/takeaway/app/App.kt
@@ -1,8 +1,12 @@
 package takeaway.app
 
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import takeaway.di.DaggerAppComponent
+
+const val YANDEX_METRICA_API_KEY = "8b206c8b-678f-49c0-953f-b653aac2f194"
 
 class App : DaggerApplication() {
 
@@ -10,4 +14,15 @@ class App : DaggerApplication() {
         DaggerAppComponent
             .factory()
             .create(this)
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Creating an extended library configuration.
+        val config = YandexMetricaConfig.newConfigBuilder(YANDEX_METRICA_API_KEY).build()
+        // Initializing the AppMetrica SDK.
+        YandexMetrica.activate(applicationContext, config)
+        // Automatic tracking of user activity.
+        YandexMetrica.enableActivityAutoTracking(this)
+    }
 }
